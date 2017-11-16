0b87b3f8a8b2c47bae120b5c1c4d8ada9daa3de8
Nicoy Caine <nicoycaine1@gmail.com>
2017-11-16 22:37:30 +0000

    Exercise 3

---
+++ world.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    $("#lookup").on('click', function(){
+        if($("#check").is(":checked")){
+            searchAll();
+        }
+        else if ($("#country").val() != ""){
+            search($("#country").val());
+        }
+    });
+});
+
+function search(query){
+    var link = 'https://info2180-lab7-nickcaine.c9users.io/world.php?country='+query;
+    $.ajax(link,{
+        method: 'GET',
+    }).done(function(res){
+        alert($(res).text());
+        $("#result").html(res);
+    }).fail(function(fail){
+        $("#result").html('<b>EPIC FAIL 500 - AJAX ERROR!</b>');
+    });
+}
+
+function searchAll(){
+    var link = 'https://info2180-lab7-nickcaine.c9users.io/world.php?all=true';
+    
+    $.ajax(link,{
+        method: 'GET',
+    }).done(function(res){
+        $("#result").html(res);
+    }).fail(function(fail){
+        $("#result").html('<b>EPIC FAIL 500 - AJAX ERROR!</b>');
+    });
+}
